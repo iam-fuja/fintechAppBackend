b88997611f6bd4b30a0a4c9b528d23200ee7f6e0
David Baba <davidbabasalifu2020.com>
2022-08-02 23:37:20 +0100

    Initial code base stup

---
+++ src/main/java/com/decagon/fintechpaymentapisqd11b/FintechPaymentApiSqd11bApplication.java
@@ -0,0 +1,13 @@
+package com.decagon.fintechpaymentapisqd11b;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FintechPaymentApiSqd11bApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FintechPaymentApiSqd11bApplication.class, args);
+	}
+
+}
