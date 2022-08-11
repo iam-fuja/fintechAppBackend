5b5c257d630d84634746a9c153e78906aa90bc74
Clemma Dev <folukeabogoh@gmail.com>
2022-08-11 21:36:21 +0100

    Includes UsersRegistrationController

--- src/main/java/com/decagon/fintechpaymentapisqd11b/controller/UsersRegistrationController.java
+++ src/main/java/com/decagon/fintechpaymentapisqd11b/controller/UsersRegistrationController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping(path = "/registration")
+@RequestMapping(path = "/api/vi/registration")
 public class UsersRegistrationController {
         public final RegistrationService registrationService;
 
